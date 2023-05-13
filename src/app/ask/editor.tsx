89dd7d8837e127126b0e2890902cfd3b2d41b96b
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-05-12 21:54:12 -0500

    Resolved TS error

--- src/app/ask/editor.tsx
+++ src/app/ask/editor.tsx
@@ -166,7 +166,7 @@ export default function Editor({ userId }: { userId: string }) {
     },
     extensions: [
       Color.configure({ types: [TextStyle.name, ListItem.name] }),
-      TextStyle.configure({ types: [ListItem.name] }),
+      TextStyle.configure({ types: [ListItem.name] } as any),
       StarterKit.configure({
         bulletList: {
           keepMarks: true,
