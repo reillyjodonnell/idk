3e3c3c28d5371d29fea13dbde0b11677a1254d98
Reilly O'Donnell <odonnell.reilly.j@gmail.com>
2023-05-15 10:41:31 -0400

    Updated editor menu bar styling

--- src/app/ask/editor-menu-bar.tsx
+++ src/app/ask/editor-menu-bar.tsx
@@ -15,6 +15,7 @@ import {
   MenubarTrigger,
 } from '@/components/menubar';
 import type { Editor as EditorType } from '@tiptap/react';
+import clsx from 'clsx';
 import {
   Bold as BoldIcon,
   Braces,
@@ -37,13 +38,16 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
     return null;
   }
   return (
-    <Menubar className="p-0 w-full sm:overflow-x-scroll sm:w-auto flex-wrap sm:flex-nowrap my-4 sm:my-0 menubar">
+    <Menubar className="overflow-y-auto h-fit p-4 w-fit xl:overflow-hidden flex-wrap lg:flex-nowrap my-4 sm:my-0 menubar">
       <MenubarMenu>
         <Button
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleBold().run()}
           disabled={!editor.can().chain().focus().toggleBold().run()}
-          className={editor.isActive('bold') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('bold') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <BoldIcon />
         </Button>
@@ -51,7 +55,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleItalic().run()}
           disabled={!editor.can().chain().focus().toggleItalic().run()}
-          className={editor.isActive('italic') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('italic') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Italic />
         </Button>
@@ -59,7 +66,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleCode().run()}
           disabled={!editor.can().chain().focus().toggleCode().run()}
-          className={editor.isActive('code') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('code') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Code />
         </Button>
@@ -67,7 +77,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
         <Button
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleCodeBlock().run()}
-          className={editor.isActive('codeBlock') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('codeBlock') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Braces />
         </Button>
@@ -75,7 +88,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
         <Button
           variant={'outline'}
           onClick={() => editor.chain().focus().setParagraph().run()}
-          className={editor.isActive('paragraph') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('paragraph') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Type />
         </Button>
@@ -84,9 +100,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 1 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 1 }) ? 'is-active' : ''
-          }
+          className={clsx(
+            editor.isActive('heading', { level: 1 }) ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Heading1 />
         </Button>
@@ -95,9 +112,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 2 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 2 }) ? 'is-active' : ''
-          }
+          className={clsx(
+            editor.isActive('heading', { level: 2 }) ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Heading2 />
         </Button>
@@ -106,9 +124,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
           onClick={() =>
             editor.chain().focus().toggleHeading({ level: 3 }).run()
           }
-          className={
-            editor.isActive('heading', { level: 3 }) ? 'is-active' : ''
-          }
+          className={clsx(
+            editor.isActive('heading', { level: 3 }) ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Heading3 />
         </Button>
@@ -116,14 +135,20 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
         <Button
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleBulletList().run()}
-          className={editor.isActive('bulletList') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('bulletList') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <List />
         </Button>
         <Button
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleOrderedList().run()}
-          className={editor.isActive('orderedList') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('orderedList') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <ListOrdered />
         </Button>
@@ -131,7 +156,10 @@ export default function EditorMenuBar({ editor }: { editor: EditorType }) {
         <Button
           variant={'outline'}
           onClick={() => editor.chain().focus().toggleBlockquote().run()}
-          className={editor.isActive('blockquote') ? 'is-active' : ''}
+          className={clsx(
+            editor.isActive('blockquote') ? 'is-active' : '',
+            'border-none'
+          )}
         >
           <Quote />
         </Button>
